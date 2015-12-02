187e68f568c1e74f78e76c8bc68f42d635561530
Desiree Kelly <desireekelly@users.noreply.github.com>
2015-12-03 01:32:40 +1100

    Add Customer.java and CustomerTest.java

---
+++ src/com/twu/biblioteca/Library/Customer.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca.Library;
+
+/**
+ * Created by Desiree Kelly on 3/12/2015.
+ */
+public class Customer {
+}
---
+++ test/com/twu/biblioteca/LibraryTests/CustomerTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca.LibraryTests;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Desiree Kelly on 3/12/2015.
+ */
+public class CustomerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
