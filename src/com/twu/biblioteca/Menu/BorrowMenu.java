d7569a08ce8ec3b4acb4f992461b9c38452af884
Desiree Kelly <desireekelly@users.noreply.github.com>
2015-12-04 17:50:30 +1100

    Code Cleanup

--- src/com/twu/biblioteca/Menu/BorrowMenu.java
+++ src/com/twu/biblioteca/Menu/BorrowMenu.java
@@ -3,36 +3,50 @@ package com.twu.biblioteca.Menu;
 import com.twu.biblioteca.Book.Book;
 import com.twu.biblioteca.Exceptions.BookNotBorrowable;
 import com.twu.biblioteca.Library.Library;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Created by Desiree Kelly on 3/12/2015.
+ * BorrowMenu is responsible for handling the borrow menu navigation.
+ *
+ * @author Desiree Kelly
+ * @version 1.0
  */
 public class BorrowMenu {
 
     private Scanner input;
     private Library library;
 
-    public BorrowMenu(Library library){
+    /**
+     * Construct a borrow menu with access to the Library.
+     */
+    public BorrowMenu(Library library) {
         this.library = library;
         input = new Scanner(System.in);
     }
 
-    public void displayBorrowMenu(){
-            try {
-                System.out.println("Select a book to borrow by entering the ID number or enter 0 to go back to the main menu:");
-                System.out.println(Utilities.displayFormattedBookList(library.getAvailableBooks()));
-                System.out.print("Enter your option:");
-                borrowMenuOptions(input.nextInt(10));
-            }
-            catch(InputMismatchException e){
-                System.out.println("\nIncorrect option, please try again.\n");
-                input.nextLine();
-            }
+    /**
+     * Display the borrow menu options.
+     */
+    public void displayBorrowMenu() {
+        try {
+            System.out.println("Select a book to borrow by entering the ID number or enter 0 to go back to the main menu:");
+            System.out.println(Utilities.displayFormattedBookList(library.getAvailableBooks()));
+            System.out.print("Enter your option:");
+            borrowMenuOptions(input.nextInt(10));
+        } catch (InputMismatchException e) {
+            System.out.println("\nIncorrect option, please try again.\n");
+            input.nextLine();
+        }
     }
 
-    public void borrowMenuOptions(int option){
+    /**
+     * Dispatch to borrow a book.
+     *
+     * @param option The option entered on the return menu.
+     */
+    public void borrowMenuOptions(int option) {
         if (option == 0) {
             System.out.print("\n");
             return;
--- src/com/twu/biblioteca/Menu/ReturnMenu.java
+++ src/com/twu/biblioteca/Menu/ReturnMenu.java
@@ -3,36 +3,50 @@ package com.twu.biblioteca.Menu;
 import com.twu.biblioteca.Book.Book;
 import com.twu.biblioteca.Exceptions.BookNotReturnable;
 import com.twu.biblioteca.Library.Library;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Created by Desiree Kelly on 3/12/2015.
+ * ReturnMenu is responsible for handling the return menu navigation.
+ *
+ * @author Desiree Kelly
+ * @version 1.0
  */
 public class ReturnMenu {
 
     private Library library;
     private Scanner input;
 
+    /**
+     * Construct a return menu with access to the Library.
+     */
     public ReturnMenu(Library library) {
         this.library = library;
         input = new Scanner(System.in);
     }
 
-    public void displayReturnMenu(){
+    /**
+     * Display the return menu options.
+     */
+    public void displayReturnMenu() {
         try {
             System.out.println("Select a book to return by entering the ID number or enter 0 to go back to the main menu:");
             System.out.println(Utilities.displayFormattedBookList(library.getBorrowedBooks()));
             System.out.print("Enter your option:");
             returnMenuOptions(input.nextInt(10));
-        }
-        catch(InputMismatchException e){
+        } catch (InputMismatchException e) {
             System.out.println("\nIncorrect option, please try again.\n");
             input.nextLine();
         }
     }
 
-    public void returnMenuOptions(int option){
+    /**
+     * Dispatch to return a book.
+     *
+     * @param option The option entered on the return menu.
+     */
+    public void returnMenuOptions(int option) {
         if (option == 0) {
             System.out.print("\n");
             return;
