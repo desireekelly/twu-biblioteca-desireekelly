d921f15209b79bbc8d36bf174a4e3d03fd7122ab
Desiree Kelly <desireekelly@users.noreply.github.com>
2015-12-07 01:22:59 +1100

    Added BorrowMenuTest and ReturnMenuTest

---
+++ test/com/twu/biblioteca/Menu/BorrowMenuTest.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca.Menu;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by desiree on 7/12/2015.
+ */
+public class BorrowMenuTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testDisplayBorrowMenu() throws Exception {
+
+    }
+
+    @Test
+    public void testBorrowMenuOptions() throws Exception {
+
+    }
+}
