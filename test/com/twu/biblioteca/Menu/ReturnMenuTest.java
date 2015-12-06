c7b7d09d8a3e1642bfd63dec24b8713c52f8c08a
Desiree Kelly <desireekelly@users.noreply.github.com>
2015-12-07 03:58:09 +1100

    Added ReturnMenu Tests

--- test/com/twu/biblioteca/Menu/ReturnMenuTest.java
+++ test/com/twu/biblioteca/Menu/ReturnMenuTest.java
@@ -1,7 +1,14 @@
 package com.twu.biblioteca.Menu;
 
+import com.twu.biblioteca.Book.Book;
+import com.twu.biblioteca.Exceptions.BookNotBorrowable;
+import com.twu.biblioteca.Exceptions.BookNotReturnable;
+import com.twu.biblioteca.Library.Library;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -25,7 +32,44 @@ public class ReturnMenuTest {
     }
 
     @Test
-    public void testReturnMenuOptions() throws Exception {
+    public void testReturnMenuKeyboardSmash() throws Exception {
 
+    }
+
+    @Test
+    public void testReturnMenuOptionsReturnBook() throws Exception {
+
+    }
+
+    @Test
+    public void testReturnMenuOptionsExit() throws Exception {
+
+    }
+
+    static class MockLibrary implements Library {
+        @Override
+        public List<Book> getAvailableBooks() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public List<Book> getBorrowedBooks() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public List<Book> getBookList() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public void checkoutBook(Book book) throws BookNotBorrowable {
+
+        }
+
+        @Override
+        public void returnBook(Book book) throws BookNotReturnable {
+
+        }
     }
 }
