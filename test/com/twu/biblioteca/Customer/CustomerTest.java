150c67b54136ac6f4de478ca2305e009c49c0743
Desiree Kelly <desireekelly@users.noreply.github.com>
2015-12-07 17:32:30 +1100

    Added Customer Tests and implemented Customer class

--- test/com/twu/biblioteca/Customer/CustomerTest.java
+++ test/com/twu/biblioteca/Customer/CustomerTest.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca.Customer;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,8 +14,48 @@ import static org.junit.Assert.*;
  */
 public class CustomerTest {
 
+    public static final Customer CUSTOMER_1 = new Customer("Joe Bloggs", "joebloggs@joebloggs.com", "0400 000 000", "123-4567", "f8kf93jd");
+    public static final Customer CUSTOMER_2 = new Customer("Jane Smith", "janesmith@janesmith.com", "0400 123 888", "123-4568", "5jgfdkl5");
+
     @Before
     public void setUp() throws Exception {
 
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        assertEquals(CUSTOMER_1.getName(), "Joe Bloggs");
+    }
+
+    @Test
+    public void testGetEmailAddress() throws Exception {
+        assertEquals(CUSTOMER_1.getEmailAddress(), "joebloggs@joebloggs.com");
+    }
+
+    @Test
+    public void testGetPhoneNumber() throws Exception {
+        assertEquals(CUSTOMER_1.getPhoneNumber(), "0400 000 000");
+    }
+
+    @Test
+    public void testGetLibraryNumber() throws Exception {
+        assertEquals(CUSTOMER_1.getLibraryNumber(), "123-4567");
+    }
+
+    @Test
+    public void testGetPassword() throws Exception {
+        assertEquals(CUSTOMER_1.getPassword(), "f8kf93jd");
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        assertEquals(CUSTOMER_1.toString(), "Joe Bloggs, joebloggs@joebloggs.com, 0400 000 000, 123-4567, f8kf93jd");
+    }
+
+    @Test
+    public void testIfTwoMoviesAreTheSame() throws Exception {
+        assertFalse(CUSTOMER_1.equals("Joe Bloggs, joebloggs@joebloggs.com, 0400 000 000, 123-4567, f8kf93jd"));
+        assertTrue(CUSTOMER_1.equals(CUSTOMER_1));
+        assertFalse(CUSTOMER_1.equals(CUSTOMER_2));
     }
 }
